e59f2c9dd4b7749c21d6f4f7eb69596be2b4de12
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2018-04-13 22:25:49 +0700

    setting CRUD for polygon in table area

--- src/repositories/Polygon.js
+++ src/repositories/Polygon.js
@@ -1,5 +1,20 @@
-
+const AreaModel = require('../models/AreaModel')
+const GeoJSONParser = require('geojson')
 
 module.exports = class Polygon {
-  
+  static async addPolygon (polygonOfGeoJSON) {
+    
+  }
+
+  static async getAllPolygon () {
+
+  }
+
+  static async updatePolygon (idPolygon, newPolygonOfGeoJSONData) {
+
+  }
+
+  static async deletePolygon (idPolygon) {
+
+  }
 }
