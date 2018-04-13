fd2cf36ff6a9b4704961b4f8f236686bc3fb107e
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2018-04-13 22:53:11 +0700

    update data polygon by id

--- src/repositories/Polygon.js
+++ src/repositories/Polygon.js
@@ -18,7 +18,13 @@ module.exports = class Polygon {
   }
 
   static async updatePolygon (idPolygon, newPolygonOfGeoJSONData) {
-
+    let polygon = Turf.polygon(newPolygonOfGeoJSONData.geometry.coordinates)
+    let area = Turf.area(polygon)
+    let update = await AreaModel.update({nama: newPolygonOfGeoJSONData.properties.nama, geom: newPolygonOfGeoJSONData.geometry, luas: area}, {where: {id: idPolygon}})
+    if (update[0] == 1) 
+      return
+    else 
+      throw "Id tidak ditemukan dan data harus berbentuk GeoJSON Polygon"
   }
 
   static async deletePolygon (idPolygon) {
