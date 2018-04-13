b030123606b03ebfe6d1c24623f2b1cecdd5a86a
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2018-04-13 22:45:32 +0700

    add turf library for calculate anything about spatial data and DONE insert polygon

--- src/repositories/Polygon.js
+++ src/repositories/Polygon.js
@@ -1,9 +1,13 @@
 const AreaModel = require('../models/AreaModel')
 const GeoJSONParser = require('geojson')
+const Turf = require('@turf/turf')
 
 module.exports = class Polygon {
   static async addPolygon (polygonOfGeoJSON) {
-    
+    let polygon = Turf.polygon(polygonOfGeoJSON.geometry.coordinates)
+    let area = Turf.area(polygon)
+    let data = await AreaModel.create({nama: polygonOfGeoJSON.properties.nama, geom: polygonOfGeoJSON.geometry, luas: area})
+    return data
   }
 
   static async getAllPolygon () {
